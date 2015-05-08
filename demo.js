b28af3c3844a88aba2132e3c0bc28c156655175c
Yura Trambitskiy <tr.yura@yahoo.com>
2015-05-08 12:11:00 +0300

    Make a demo

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
 var cascadeElement = document.querySelector('.cascade');
 var cascade = new Cascade(cascadeElement);
 
-var addButton = document.querySelector('.add-10');
+var addButton = document.querySelector('.add-5');
 addButton.onclick = function() {
-	for (var i=0; i<10; i++) {
+	for (var i=0; i<5; i++) {
 		var child = cascadeElement.children[i];
 		var clone = child.cloneNode(true);
 		cascadeElement.appendChild(clone);
