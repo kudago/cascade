d30c52f6abf5d47c95b7c8a02f2e34f5acd9f626
Yura Trambitskiy <tr.yura@yahoo.com>
2015-05-08 12:08:58 +0300

    Fix demo numbers to 5

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 var cascadeElement = document.querySelector('.cascade');
 var cascade = new Cascade(cascadeElement);
 
-var addButton = document.querySelector('.add-10');
+var addButton = document.querySelector('.add-5');
 addButton.onclick = function() {
 	for (var i=0; i<5; i++) {
 		var child = cascadeElement.children[i];
